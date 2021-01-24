d324fc9846118f8aeec720459320747e91156bad
Joan <joancardona.17@gmail.com>
2021-01-24 11:12:19 +0100

    Initial Commit

---
+++ RevenueCatTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RevenueCatTracker
+//
+//  Created by Joan Cardona on 24/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
